39adfe096f7875f4b59214f9c0756fa4372e45cb
Effy <9110264+Temhr@users.noreply.github.com>
2024-10-03 15:53:21 -0400

    Create bs.sh

---
+++ bs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+sudo timedatectl set-timezone Canada/Eastern
+
+sudo NEEDRESTART_MODE=a apt update && apt dist-upgrade -y
+
+sudo NEEDRESTART_MODE=a apt install qemu-guest-agent -y
+
+sudo NEEDRESTART_MODE=a apt install git -y
+
+sudo NEEDRESTART_MODE=a apt install ansible -y
+
+sudo ansible-pull -U https://github.com/Temhr/Ansi.git
