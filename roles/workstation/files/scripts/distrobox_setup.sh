f75ef09ad2b4bfe61df550e764cc8f6f70db8d55
Effy <9110264+Temhr@users.noreply.github.com>
2024-10-09 04:02:47 -0400

    Update distrobox_setup.sh

--- roles/workstation/files/scripts/distrobox_setup.sh
+++ roles/workstation/files/scripts/distrobox_setup.sh
@@ -5,5 +5,3 @@ mkdir /home/temhr/containers/steam
 distrobox create --home /home/temhr/containers/steam --name steamos --image ghcr.io/linuxserver/steamos:latest --hostname steamos
 
 distrobox enter steamos
-
-distrobox-export --app steam
