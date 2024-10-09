e611e678d4617902bf83e1edcb9f33cd54343e46
Effy <9110264+Temhr@users.noreply.github.com>
2024-10-09 05:20:26 -0400

    Delete roles/workstation/files/scripts/distrobox_steamos.sh

--- roles/workstation/files/scripts/distrobox_steamos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir /home/temhr/containers/steamos
-
-distrobox create --home /home/temhr/containers/steamos --name steamos --image ghcr.io/linuxserver/steamos:latest --hostname steamos
-
-printf '\n'
-
-distrobox enter steamos
