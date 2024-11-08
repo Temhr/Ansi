1a9b8a68fbdd8fb5d162b3af20b4ed5e7f6fdae0
Effy <9110264+Temhr@users.noreply.github.com>
2024-11-08 14:33:35 -0500

    Update bs.sh

--- bs.sh
+++ bs.sh
@@ -2,7 +2,7 @@
 
 sudo timedatectl set-timezone Canada/Eastern
 
-sudo NEEDRESTART_MODE=a apt update && apt dist-upgrade -y
+sudo NEEDRESTART_MODE=a date >> /home/temhr/Ansi.txt && apt update >> /home/temhr/Ansi.txt && apt dist-upgrade -y >> /home/temhr/Ansi.txt
 
 sudo NEEDRESTART_MODE=a apt install qemu-guest-agent -y
 
@@ -10,6 +10,6 @@ sudo NEEDRESTART_MODE=a apt install git -y
 
 sudo NEEDRESTART_MODE=a apt install ansible -y
 
-sudo ansible-pull -U https://github.com/Temhr/Ansi.git | tee /home/temhr/initLog.txt
+sudo ansible-pull -U https://github.com/Temhr/Ansi.git | tee /home/temhr/AnsiLog.txt
 
 sudo reboot
